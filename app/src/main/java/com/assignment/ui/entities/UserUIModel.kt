4dc29736c69925d9a83e5c8937ac7819e73f3f9c
Prathaalbasicmsatam67 <prathamsatam67@gmail.com>
2021-10-25 00:28:20 +0530

    Delete UserUIModel.kt
    Removed unused code

--- app/src/main/java/com/assignment/ui/entities/UserUIModel.kt
+++
@@ -1,8 +0,0 @@
-package com.assignment.ui.entities
-
-data class UserUIModel(  val name: String,
-                         val age : Int,
-                         val city : String,
-                         val picURL: String,
-                         val serverUUID: String?,
-                         val userApproval: Int)
